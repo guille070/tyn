c4e7dbf0263844612eabb67a4f01e502ac3f64e8
Guillermo López <guille070@gmail.com>
2021-06-23 11:47:55 +0200

    Renombrado bloques reutilizables

--- wp-content/themes/tynmagazine/theme_core/functions/functions.php
+++ wp-content/themes/tynmagazine/theme_core/functions/functions.php
@@ -254,10 +254,10 @@ add_filter('block_editor_settings', function ($editor_settings) {
 });
 
 /**
-* Add Reusable Blocks to admin menu
+* Add Bloques Reutilizables to admin menu
 */
 function add_reusable_blocks_admin_menu() {
-    add_menu_page( 'Reusable Blocks', 'Reusable Blocks', 'edit_posts', 'edit.php?post_type=wp_block', '', 'dashicons-editor-table', 22 );
+    add_menu_page( 'Bloques Reutilizables', 'Bloques Reutilizables', 'edit_posts', 'edit.php?post_type=wp_block', '', 'dashicons-editor-table', 22 );
 }
 add_action( 'admin_menu', 'add_reusable_blocks_admin_menu' );
 
