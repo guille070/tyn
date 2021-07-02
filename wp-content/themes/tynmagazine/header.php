a4b3b2ec96a41a2719fa0bc0e69de1fa7ef8c359
Guillermo López <guille070@gmail.com>
2021-07-02 11:06:58 +0200

    Eliminado de page preloader

--- wp-content/themes/tynmagazine/header.php
+++ wp-content/themes/tynmagazine/header.php
@@ -22,19 +22,6 @@
         <!-- Page -->
         <div class="text-center page">
 
-            <!-- Page preloader-->
-            <div class="page-loader">
-                <div>
-                    <div class="page-loader-body">
-                        <div class="cssload-loader">
-                            <div class="cssload-inner cssload-one"></div>
-                            <div class="cssload-inner cssload-two"></div>
-                            <div class="cssload-inner cssload-three"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <?php theme_get_header(); ?>
 
         
