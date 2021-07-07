79f906389830b60a65e9819799c65c6c0de33ce6
Guillermo López <guille070@gmail.com>
2021-07-07 20:29:35 +0200

    Update de bloques no utilizadas (unregisterBlockType)

--- wp-content/themes/tynmagazine/js/theme.js
+++ wp-content/themes/tynmagazine/js/theme.js
@@ -1,3 +1,20 @@
 wp.domReady( function () {
-    wp.blocks.unregisterBlockType( 'core/latest-posts' );
+    var unregister_blocks = [
+        'core/latest-posts', 
+        'core/latest-comments', 
+        'core/rss', 
+        'core/calendar', 
+        'core/social-link', 
+        'core/social-links', 
+        'core/categories', 
+        'custom-banners/banner-list',
+        'custom-banners/rotating-banner',
+        'custom-banners/single-banner'
+    ];
+
+    if (unregister_blocks) {
+        unregister_blocks.forEach(element => {
+            wp.blocks.unregisterBlockType( element );
+        });
+    }
 } );
